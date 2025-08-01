c22e607a9bf20b78a719ccba774b611fec8e4ead
mister avestruz <pastochenko.soares@escola.pr.gov.br>
2025-08-01 12:05:29 -0300

    m

--- script.js
+++ script.js
@@ -5,7 +5,26 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const palavras = texto.split(" ");
+    const palavrasChave = processaTexto (texto);
 
     campoResultado.textContent = palavras.join(", ");
+}
+
+function processaTexto(texto){
+    let palavra = texto. split (/\P{L}+/u);
+    
+    
+  let  frequencias = [];  
+    for( let i in palavras ){
+       frequencias[i] = 0;
+    for (let j in palavras);{
+        
+    }
+
+    }
+    
+   console.log(frequencias);
+    
+
+    return  palavras;
 }
